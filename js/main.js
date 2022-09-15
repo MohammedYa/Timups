dfb15fe8bc9a74c7ff7748fb1def3bc40bec3e5f
MohammedYa <mohamedel3sawy4110@gmail.com>
2022-09-15 21:04:07 +0200

    upload project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop:true,
+            margin:40,
+           
+            items:2,
+            autoplay:true
+     
+        }
+    );
+  });
+ 
